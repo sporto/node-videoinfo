26da2ce2b42ecf8d21b76f656cc08f37ce93362b
Sebastian <sebasporto@gmail.com>
2013-03-24 21:23:49 +1100

    tests

--- test/main_test.js
+++ test/main_test.js
@@ -3,15 +3,8 @@ var assert = require('chai').assert;
 
 describe('main', function () {
 
-	it('has a scrape function', function () {
-		assert.isFunction(main.scrape);
-	});
-
-	it('loads the page', function (done) {
-		// main.scrape('http://www.confreaks.com/videos/1288-rubyconf2012-ruby-vs-the-world', function (err, res) {
-		// 	assert.equal(res.title, 'Ruby vs. the world');
-		// 	done();
-		// });
+	it('has a run function', function () {
+		assert.isFunction(main.run);
 	});
 
 });
