c9f0b75c9b26e2d287e12eb37be60c2e7949a55a
Sebastian <sebasporto@gmail.com>
2013-03-29 00:34:27 +1100

    added provider and provideId to results

--- full_test/providers.js
+++ full_test/providers.js
@@ -6,3 +6,6 @@ console.log(main.getId('http://vimeo.com/26355165'));
 console.log(main.getProvider('http://www.youtube.com/watch?v=9eHbqMayxv8'));
 console.log(main.getId('http://www.youtube.com/watch?v=9eHbqMayxv8'));
 
+console.log(main.getProvider('http://www.confreaks.com/videos/1288-rubyconf2012-ruby-vs-the-world'));
+console.log(main.getId('http://www.confreaks.com/videos/1288-rubyconf2012-ruby-vs-the-world'));
+
