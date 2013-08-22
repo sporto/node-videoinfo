752955fd0925c12b048d0e07f264d4c42c63f037
David Knell <dk45@mac.com>
2013-08-22 11:59:52 -0400

    added support for Youtube and Vimeo embed URLs

--- real_test.js
+++ real_test.js
@@ -10,6 +10,16 @@ main.fetch('http://www.youtube.com/watch?v=9eHbqMayxv8', function (err, data) {
 	console.log(data);
 });
 
+main.fetch('http://player.vimeo.com/video/70228559?title=0&portrait=0', function (err, data) {
+	console.log('==VIMEO ALT URL FORMAT==')
+	console.log(data);
+});
+
+main.fetch('http://www.youtube.com/v/Gz18KskDxv4&fs=1&rel=0&wmode=opaque&ap=%2526fmt%3D18', function (err, data) {
+	console.log('==YOUTUBE ALT URL FORMAT==');
+	console.log(data);
+});
+
 main.fetch('http://www.confreaks.com/videos/1288-rubyconf2012-ruby-vs-the-world', function (err, data) {
 	console.log('==CONFREAKS==');
 	console.log(data);
