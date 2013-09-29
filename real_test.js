3035ab829fc7582ea8f4d5442cff211c46be0893
Sebastian <sebasporto@gmail.com>
2013-09-29 20:15:52 +1000

    Added more test for id

--- real_test.js
+++ real_test.js
@@ -10,11 +10,6 @@ main.fetch('http://www.youtube.com/watch?v=9eHbqMayxv8', function (err, data) {
 	console.log(data);
 });
 
-main.fetch('http://player.vimeo.com/video/70228559?title=0&portrait=0', function (err, data) {
-	console.log('==VIMEO ALT URL FORMAT==')
-	console.log(data);
-});
-
 main.fetch('http://www.youtube.com/v/Gz18KskDxv4&fs=1&rel=0&wmode=opaque&ap=%2526fmt%3D18', function (err, data) {
 	console.log('==YOUTUBE ALT URL FORMAT==');
 	console.log(data);
@@ -22,6 +17,11 @@ main.fetch('http://www.youtube.com/v/Gz18KskDxv4&fs=1&rel=0&wmode=opaque&ap=%252
 
 main.fetch('http://www.youtube.com/embed/0bmbGsJDE1o?fs=1&rel=0&wmode=opaque&ap=%2526fmt%3D18', function (err, data) {
 	console.log('==YOUTUBE EMBED URL FORMAT==');
+	console.log(data);
+});
+
+main.fetch('http://player.vimeo.com/video/70228559?title=0&portrait=0', function (err, data) {
+	console.log('==VIMEO ALT URL FORMAT==')
 	console.log(data);
 });
 
